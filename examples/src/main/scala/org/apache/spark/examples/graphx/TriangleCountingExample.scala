07c6475ea986aeafe9495fa7fdc91542a3242069
jeongyoon <jeongyoon.eo@snu.ac.kr>
2020-05-14 04:58:09 +0000

    svdpp

--- examples/src/main/scala/org/apache/spark/examples/graphx/TriangleCountingExample.scala
+++ examples/src/main/scala/org/apache/spark/examples/graphx/TriangleCountingExample.scala
@@ -49,11 +49,12 @@ object TriangleCountingExample {
 
     // $example on$
     // Load the edges in canonical order and partition the graph for triangle count
-    val graph = GraphLoader.edgeListFile(sc, "data/graphx/followers.txt", true)
+    val graph = GraphLoader.edgeListFile(sc, "data/graphx/twitter", true)
       .partitionBy(PartitionStrategy.RandomVertexCut)
     // Find the triangle count for each vertex
     val triCounts = graph.triangleCount().vertices
     // Join the triangle counts with the usernames
+    /*
     val users = sc.textFile("data/graphx/users.txt").map { line =>
       val fields = line.split(",")
       (fields(0).toLong, fields(1))
@@ -63,6 +64,7 @@ object TriangleCountingExample {
     }
     // Print the result
     println(triCountByUsername.collect().mkString("\n"))
+    */
     // $example off$
     spark.stop()
   }
