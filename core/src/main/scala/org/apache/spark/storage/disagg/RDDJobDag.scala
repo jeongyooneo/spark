873b7de729fec0cfd9c45781bd5514b02d8d9d9e
taegeonum <taegeonum@gmail.com>
2020-12-11 20:30:12 +0900

    exclude starting stage

--- core/src/main/scala/org/apache/spark/storage/disagg/RDDJobDag.scala
+++ core/src/main/scala/org/apache/spark/storage/disagg/RDDJobDag.scala
@@ -625,8 +625,8 @@ class RDDJobDag(val dag: mutable.Map[RDDNode, mutable.Set[RDDNode]],
 
 
           if (!stageSet.contains(childnode.rootStage) &&
-            !metricTracker.completedStages.contains(childnode.rootStage)
-          && !metricTracker.stageStartTime.contains(childnode.rootStage)) {
+            !metricTracker.completedStages.contains(childnode.rootStage)) {
+          // && !metricTracker.stageStartTime.contains(childnode.rootStage)) {
             stageSet.add(childnode.rootStage)
             map.put(childnode.rootStage,
               new StageDistance(childnode.rootStage, distance, absolute, prevc))
@@ -634,8 +634,8 @@ class RDDJobDag(val dag: mutable.Map[RDDNode, mutable.Set[RDDNode]],
           }
 
           if (!metricTracker.completedStages.contains(childnode.rootStage)
-            && !metricTracker.stageStartTime.contains(childnode.rootStage)
             && dag(childnode).size >= 2) {
+            // && !metricTracker.stageStartTime.contains(childnode.rootStage)
             // finish
           } else {
             // if (getRefCntRDD(childnode.rddId) < 2) {
