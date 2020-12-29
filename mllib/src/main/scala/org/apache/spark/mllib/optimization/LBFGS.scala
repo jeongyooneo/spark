01e80d417328ae230cbb987bd74339706f8bbee7
taegeonum <taegeonum@gmail.com>
2020-12-30 08:38:36 +0900

    lr param

--- mllib/src/main/scala/org/apache/spark/mllib/optimization/LBFGS.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/optimization/LBFGS.scala
@@ -41,9 +41,9 @@ class LBFGS(private var gradient: Gradient, private var updater: Updater)
   extends Optimizer with Logging {
 
   private var numCorrections = 10
-  private var convergenceTol = 1E-6
+  private var convergenceTol = 0.1
   private var maxNumIterations = 100
-  private var regParam = 0.0
+  private var regParam = 0.2
 
   /**
    * Set the number of corrections used in the LBFGS update. Default 10.
