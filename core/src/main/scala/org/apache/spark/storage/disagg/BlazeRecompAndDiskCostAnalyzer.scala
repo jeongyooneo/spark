7a3f7d01541cc0029e08153da63853c300f7a99e
taegeonum <taegeonum@gmail.com>
2021-01-04 00:25:15 +0900

    debug

--- core/src/main/scala/org/apache/spark/storage/disagg/BlazeRecompAndDiskCostAnalyzer.scala
+++ core/src/main/scala/org/apache/spark/storage/disagg/BlazeRecompAndDiskCostAnalyzer.scala
@@ -107,7 +107,7 @@ private[spark] class BlazeRecompAndDiskCostAnalyzer(val rddJobDag: RDDJobDag,
             result = node.crossReferenced && node.jobId + 1 > metricTracker.currJob.get()
           }
 
-          logInfo(s"No repeatedNode for ${node.rddId}, " +
+          logDebug(s"No repeatedNode for ${node.rddId}, " +
             s"check conseuctive job reference, " +
             s"currjob ${metricTracker.currJob.get()}, " +
             s"refJob ${rddJobDag.getReferencedJobs(node)}, " +
